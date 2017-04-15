0ba8ff2d0a1f9b1fe5adad1b0110ac9763cf2d10
RicardoWX <wangxudaren@icloud.com>
2017-04-15 11:10:59 +0800

    Revert "Chat0.4"
    This reverts commit 84bdbc02e31b06b851be13b2b0dfefa4acc8a873.

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -2,8 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.TextArea;
 import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -17,6 +15,7 @@ public class ChatClient extends Frame{
 	TextArea taContent=new TextArea();
 	
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		new ChatClient().lanuchFrame();
 	}
 	
@@ -35,21 +34,9 @@ public class ChatClient extends Frame{
 			}
         	
         });
-		taText.addActionListener(new TFListener());
-        pack();
+		pack();
 		
-	}
-	
-	private class TFListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String s=taText.getText().trim();
-			taContent.setText(s);
-			taText.setText("");
 			
-		}
-		
 		
 	}
 
